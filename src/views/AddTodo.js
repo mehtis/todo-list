7fe76672ff74641b5cc8bdcd157c9c2e7a72fe79
Niko Mehtonen <nikomehtonen@gmail.com>
2019-07-28 15:26:24 +0300

    Part of the AddNote structure

--- src/views/AddTodo.js
+++ src/views/AddTodo.js
@@ -4,12 +4,36 @@ import Hamburger from '../components/Hamburger'
 
 import '../css/AddTodo.css'
 
-const AddTodo = () => {
+const AddTodo = props => {
+  const onSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="addTodo">
+    <form onSubmit={onSubmit}>
       <Hamburger right noOverlay />
       <h1>Add todo</h1>
-    </div>
+      <div className="div-add-items">
+        <div className="div-input-line">
+          <h2>Title</h2>
+          <input
+            type="text"
+            name="title"
+            placeholder="Write title"
+          />
+        </div>
+        <div className="div-input-line">
+          <h2>Deadline</h2>
+          <input
+            type="text"
+            name="Deadline"
+            placeholder="19.7.2019"
+          />
+        </div>
+        <button type="submit">Add</button>
+      </div>
+
+    </form>
   )
 }
 
